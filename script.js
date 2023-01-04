e66ec69023de06960c991522c969a5a98d562d23
Mayur-prajapati9094 <121927522+Mayur-prajapati9094@users.noreply.github.com>
2023-01-04 13:53:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let x = 10 , y = 20 , z = x + y;
+document.write(z);
+
+//age------------
+
+/*let a = prompt("Enter your age");
+if(a < 18){
+    window.alert("You are not capable")
+}else{
+    window.alert("You are capable")
+}*/
+
+
+
+
+//Time------------
+
+/*let time = prompt("Please enter your time");
+
+if(time > 0 && time < 12){
+    window.alert("Good Morning")
+}else if(time == 12){
+    window.alert("Good Noon");
+}else if (time > 12 && time <= 17 ){
+    window.alert("Good afternoon")
+}else if(time > 17 && time <= 21){
+    window.alert("Good evening");
+}else if(time > 21 && time <= 24){
+    window.alert("Good night");
+}else{
+    window.alert("Please enter valid time");
+}*/
+
+
+
